ed578964acf4a7ebcf13c726df5dfb8fc8e1c806
thesis <1711327451@qq.com>
2021-01-08 11:45:50 +0800

    style(CountUp): 添加flexbox布局，一边显示效果,一边显示源代码

--- src/pages/countUp/index.tsx
+++ src/pages/countUp/index.tsx
@@ -2,8 +2,41 @@ import  React, { useState, useEffect } from 'react';
 import CountUp, { useCountUp } from 'react-countup';
 import { Button, Divider, message } from 'antd';
 import './index.css';
-//import styles from './index.css';
 
+const renderCountUpByHook = `
+const {
+    countUp,
+    start,
+    pauseResume,
+    reset,
+    update
+} = useCountUp({
+    start: 0,
+    end: 23,
+    delay: 2,
+    duration: 5,
+    onReset: () => message.info('onReset'),      
+    onStart: () => message.info('onStart'),      
+    onUpdate: () => message.info('onUpdate'),   
+    onPauseResume: () => message.info('onPauseResume')
+});
+ 
+return(
+    <div>
+        <div className='title-container'><h1>React-countUp demo</h1></div>
+        <div className="site-button-ghost-wrapper">
+            <div className="site-button-ghost-item">
+                <div>{ countUp }</div>
+                <Button onClick={ start }>Start</Button>
+                <Button onClick={ pauseResume }>pauseResume</Button>
+                <Button onClick={ reset }>reset</Button>
+                <Button onClick={() => update(50) }>update to 50</Button>
+            </div>
+            <div className="site-button-ghost-item">
+            </div>
+        </div>    
+    </div>
+)`;
 const CountUpTip = () => {
     const {
         countUp,
@@ -23,21 +56,21 @@ const CountUpTip = () => {
     });
      
     return(
-        // <div className={styles.site-button-ghost-wrapper}>
-        <div className="site-button-ghost-wrapper" style={{width: 1000}}>
+        <div>
+            <div className='title-container'><h1>React-countUp demo</h1></div>
             <div className="site-button-ghost-wrapper">
-                <div>{ countUp }</div>
-                <Button onClick={ start }>Start</Button>
-                <Button onClick={ pauseResume }>pauseResume</Button>
-                <Button onClick={ reset }>reset</Button>
-                <Button onClick={() => update(50) }>update to 50</Button>
-            </div>
-            <Divider type="vertical" dashed/>
-            <div className="site-button-ghost-wrapper">
-
-            </div>
-        </div>    
-
+                <div className="site-button-ghost-item">
+                    <div>{ countUp }</div>
+                    <Button onClick={ start }>Start</Button>
+                    <Button onClick={ pauseResume }>pauseResume</Button>
+                    <Button onClick={ reset }>reset</Button>
+                    <Button onClick={() => update(50) }>update to 50</Button>
+                </div>
+                <div className="site-button-ghost-item">
+                    {renderCountUpByHook}
+                </div>
+            </div>    
+        </div>
     )
 }
-export default CountUpTip
+export default CountUpTip;
