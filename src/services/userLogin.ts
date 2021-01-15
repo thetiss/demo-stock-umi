7eec215ecc991a43025a35bef11d4c1ec7ddc02c
thesis <1711327451@qq.com>
2021-01-15 13:42:15 +0800

    style(pages): 调整登录界面元素样式

---
+++ src/services/userLogin.ts
@@ -0,0 +1,23 @@
+import { extend } from 'umi-request';
+import { message } from 'antd';
+import { IUserLoginForm } from '../pages/account/login';
+
+const errorHandler = function (error: any) {
+    if(error.response){
+        console.log(error.response);
+        if(error.response.status > 400){
+            console.log(error.data)
+            message.error(error.data.message?error.data.message:error.data)
+        }else{
+            message.error('Network Error')
+        }
+    }
+    throw error;
+}
+const extendRequest = extend({errorHandler});
+
+export const userLogin = async ( params: IUserLoginForm ) => {
+    return extendRequest('/api/manage/user/login.do',{
+        params
+    })
+}
